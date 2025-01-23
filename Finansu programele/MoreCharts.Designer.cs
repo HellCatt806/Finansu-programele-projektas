7b7c6655519a7969eae5e9b852796f398b86b3d8
HellCatt806 <105662956+HellCatt806@users.noreply.github.com>
2025-01-23 16:05:16 +0200

    V 1.0

--- Finansu programele/MoreCharts.Designer.cs
+++ Finansu programele/MoreCharts.Designer.cs
@@ -29,35 +29,60 @@
         private void InitializeComponent()
         {
             this.closeButton = new System.Windows.Forms.Button();
-            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.lineChart = new LiveCharts.WinForms.CartesianChart();
+            this.barChart = new LiveCharts.WinForms.CartesianChart();
+            this.savingsChart = new LiveCharts.WinForms.CartesianChart();
             this.SuspendLayout();
             // 
             // closeButton
             // 
-            this.closeButton.Location = new System.Drawing.Point(551, 553);
+            this.closeButton.Location = new System.Drawing.Point(557, 581);
+            this.closeButton.Margin = new System.Windows.Forms.Padding(2);
             this.closeButton.Name = "closeButton";
-            this.closeButton.Size = new System.Drawing.Size(113, 43);
+            this.closeButton.Size = new System.Drawing.Size(75, 30);
             this.closeButton.TabIndex = 0;
             this.closeButton.Text = "Uždaryti";
             this.closeButton.UseVisualStyleBackColor = true;
             this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
             // 
-            // cartesianChart1
+            // lineChart
             // 
-            this.cartesianChart1.Location = new System.Drawing.Point(783, 51);
-            this.cartesianChart1.Name = "cartesianChart1";
-            this.cartesianChart1.Size = new System.Drawing.Size(350, 258);
-            this.cartesianChart1.TabIndex = 1;
-            this.cartesianChart1.Text = "cartesianChart1";
+            this.lineChart.Location = new System.Drawing.Point(11, 395);
+            this.lineChart.Margin = new System.Windows.Forms.Padding(2);
+            this.lineChart.Name = "lineChart";
+            this.lineChart.Size = new System.Drawing.Size(1112, 182);
+            this.lineChart.TabIndex = 1;
+            this.lineChart.Text = "cartesianChart1";
+            // 
+            // barChart
+            // 
+            this.barChart.Location = new System.Drawing.Point(11, 32);
+            this.barChart.Margin = new System.Windows.Forms.Padding(2);
+            this.barChart.Name = "barChart";
+            this.barChart.Size = new System.Drawing.Size(593, 339);
+            this.barChart.TabIndex = 2;
+            this.barChart.Text = "cartesianChart2";
+            // 
+            // savingsChart
+            // 
+            this.savingsChart.Location = new System.Drawing.Point(671, 32);
+            this.savingsChart.Margin = new System.Windows.Forms.Padding(2);
+            this.savingsChart.Name = "savingsChart";
+            this.savingsChart.Size = new System.Drawing.Size(452, 339);
+            this.savingsChart.TabIndex = 3;
+            this.savingsChart.Text = "cartesianChart2";
             // 
             // MoreCharts
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1188, 608);
-            this.Controls.Add(this.cartesianChart1);
+            this.ClientSize = new System.Drawing.Size(1160, 622);
+            this.Controls.Add(this.savingsChart);
+            this.Controls.Add(this.barChart);
+            this.Controls.Add(this.lineChart);
             this.Controls.Add(this.closeButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "MoreCharts";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Finansų programėlė";
@@ -69,6 +94,8 @@
         #endregion
 
         private System.Windows.Forms.Button closeButton;
-        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+        private LiveCharts.WinForms.CartesianChart lineChart;
+        private LiveCharts.WinForms.CartesianChart barChart;
+        private LiveCharts.WinForms.CartesianChart savingsChart;
     }
 }
